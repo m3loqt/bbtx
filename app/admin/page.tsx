daa616f63ea56f96f83fde77f523a89320072388
Mel Angelo <mees.cortes.swu@phinmaed.com>
2026-03-18 05:43:21 +0800

    Pre-render Admin Pages fixed

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { Suspense } from 'react'
 import Link from 'next/link'
 import { getDashboardStats, getSubmissionChartData, getNewsletterChartData, getContactSubmissions, getAssessmentSubmissions } from '@/lib/admin/queries'
